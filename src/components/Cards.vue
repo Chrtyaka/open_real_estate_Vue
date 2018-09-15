e56f0061d4c5ad8d54abdfe9471eb16dda30c3c6
Chrtyaka <chrtyaka@gmail.com>
2018-09-15 14:15:45 +0300

    Doesn't matter

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -3,7 +3,8 @@
       <div class="card z-depth-0">
         <div class="row">
           <div class="col-md-4">
-            <img :src="object.urlThumbImage" onerror="this.src='https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97400&w=400&h=400'"  style="height: 200px; width: 250px;">
+            <img :src="object.urlThumbImage"
+                 onerror="this.src='https://placeholdit.imgix.net/~text?txtsize=38&txt=400%C3%97400&w=400&h=400'"  style="height: 200px; width: 250px;">
           </div>
           <div class="col-md-8 px-3">
             <div class="card-block px-3">
