5ad3c3bb67c9552f9d0e15541d80f04a083f28a9
Chrtyaka <chrtyaka@gmail.com>
2018-09-20 19:52:57 +0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -2,26 +2,36 @@ import Home from "./components/Home"
 import Objects from "./components/Objects"
 import Map from "./components/Map"
 import Price from "./components/Price"
+import City from "./components/City"
+
 export const routes = [
-  {path : '/home', name : 'home ', components : {
+  {
+    path: '/home', name: 'home ', components: {
       default: Home,
-    }},
-  {path : '/objects', name : 'objects', components:  {
-    default : Objects
-}},
-  {path : '/objects/:tabname', name : 'objects', components:  {
-      default : Objects
-    }, props : true},
-  {path : '/map', name : 'map', components:  {
-      default : Map
-    }},
-  {path : '/price', name : 'price', components:  {
-      default : Price
-    }},
-  {path : '/price/:tabname', name : 'price', components:  {
-      default : Price
-    }, props : true},
-  {path : '', redirect : '/home'},
-  {path : '*', redirect: {name : 'home'}
+    }
+  },
+  {
+    path: '/objects', name: 'objects', components: {
+      default: Objects
+    }
+  },
+  {
+    path: '/city', name: 'city', components: {
+      default: City
+    }
+  },
+  {
+    path: '/map', name: 'map', components: {
+      default: Map
+    }
+  },
+  {
+    path: '/price', name: 'price', components: {
+      default: Price
+    }
+  },
+  {path: '', redirect: '/home'},
+  {
+    path: '*', redirect: {name: 'home'}
   }
 ];
