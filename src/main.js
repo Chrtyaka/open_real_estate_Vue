a2fc027095f27956ac7c471fa2efbde4cf79ecb2
Chrtyaka <chrtyaka@gmail.com>
2018-09-20 19:50:04 +0300

    remove packages

--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import {routes} from "./routes";
-import HighChartsVue from "highcharts-vue"
 import "leaflet/dist/leaflet.css"
-import VueTabs from 'vue-nav-tabs'
-import 'vue-nav-tabs/themes/material.css'
 import {store} from "./store/store";
 
 delete L.Icon.Default.prototype._getIconUrl;
@@ -18,14 +15,15 @@ L.Icon.Default.mergeOptions({
 });
 
 
-Vue.use(VueTabs);
 Vue.use(VueResource);
 Vue.use(VueRouter);
-Vue.use(HighChartsVue);
 Vue.http.options.root = 'http://127.0.0.1:5000/';
 Vue.http.headers.common['Access-Control-Allow-Origin'] = '*';
 const router = new VueRouter({
   routes,
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
   mode: 'history'
 });
 
