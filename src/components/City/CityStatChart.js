2e4ab7cbcaf11967b289fa12841f8d601ad41350
Chrtyaka <chrtyaka@gmail.com>
2018-09-29 23:39:19 +0300

    Update component

--- src/components/City/CityStatChart.js
+++ src/components/City/CityStatChart.js
@@ -6,21 +6,5 @@ export default {
   props : ['options'],
   mounted() {
     this.renderChart(this.chartData, this.options);
-  },
-  // computed: {
-  //   chartData: function() {
-  //     return this.data;
-  //   }
-  // },
-  // methods : {
-  //   renderLineChart() {
-  //     this.renderChart(this.chartData, this.options)
-  //   }
-  // },
-  // watch: {
-  //   data: function() {
-  //     this.$data._chart.destroy();
-  //     this.renderChart(this.data, this.options);
-  //   }
-  // }
+  }
 }
